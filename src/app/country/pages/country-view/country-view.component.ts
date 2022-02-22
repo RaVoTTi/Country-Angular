36351655eaf38b4c4b1e8f9259c887f6e560e91e
RaVoTTi <valenravo@gmail.com>
2022-02-22 17:55:45 -0300

    first commit

---
+++ src/app/country/pages/country-view/country-view.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-country-view',
+  templateUrl: './country-view.component.html',
+  styles: [
+  ]
+})
+export class CountryViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
